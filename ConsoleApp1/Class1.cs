1f94bf38a9d56710f5c6aaa7beb8cf0d36412285
David Barredo <datdat@rdmsinc.com>
2023-11-19 20:24:41 +0800

    add console test

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Class1
+    {
+        public void getSomething()
+        {
+            Console.WriteLine("I got something.");
+        }
+
+        public void getNothing()
+        {
+            Console.WriteLine("I got nothing!");
+        }
+
+        public int sumNum(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            writeStuffFirst();
+            writeStuffSecond();
+
+            Class1 class1 = new Class1();
+            var a = class1.sumNum(1, 2);
+
+            Console.WriteLine(a);
+
+            Console.ReadLine();
+        }
+
+        private static void writeStuffFirst()
+        {
+            Console.WriteLine("Hello");
+            Class1 c1 = new Class1();
+            c1.getSomething();
+        }
+
+        private static void writeStuffSecond()
+        {
+            Console.WriteLine("World");
+            Class1 class1 = new Class1();
+            class1.getNothing();
+        }
+    }
+}
