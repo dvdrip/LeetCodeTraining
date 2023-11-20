f91bbfeb7ff91fb20aefee935f56466b63ee7571
David Barredo <datdat@rdmsinc.com>
2023-11-20 15:49:10 +0800

    add palindrome

---
+++ Palindrome Number/Palindrome.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome_Number
+{
+    public class Palindrome
+    {
+        public bool IsPalindrome(int x)
+        {
+            int original = x;
+            int reverse = 0;
+
+            while (x > 0)
+            {
+                int y = x % 10;
+                Console.WriteLine($"Num is {y}");
+                reverse = reverse * 10 + y;
+                Console.WriteLine($"Reverse is {reverse}");
+                x = x / 10;
+            }
+
+            return original == reverse;
+        }
+    }
+}
---
+++ Palindrome Number/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome_Number
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int myNum = 12321;
+
+            Palindrome palindrome = new Palindrome();
+
+            var myBool = palindrome.IsPalindrome(myNum);
+
+            Console.WriteLine(myBool);
+
+            Console.ReadLine();
+        }
+
+    }
+}
