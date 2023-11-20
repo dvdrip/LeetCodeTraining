eb75cc8967609443ab58d06b5d88a6d4488d33c1
David Barredo <datdat@rdmsinc.com>
2023-11-21 01:04:40 +0800

    add reverse string initial submission

---
+++ Reverse String/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] s = { 'h', 'e', 'l', 'l', 'o' };
+
+            Solution solution = new Solution();
+            solution.ReverseString(s);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Reverse String/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse_String
+{
+    internal class Solution
+    {
+        public void ReverseString(char[] s)
+        {
+            string reversedString = "";
+
+            for (int i = s.Length - 1; i >= 0; i--) {
+
+                reversedString += s[i];
+            }
+
+            char[] reversedChar = reversedString.ToCharArray();
+
+            Console.WriteLine(reversedChar);
+        }
+    }
+}
