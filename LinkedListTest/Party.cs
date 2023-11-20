2d7e6d69e2ec1e15a92ec9710fccba9314dda88d
David Barredo <datdat@rdmsinc.com>
2023-11-20 22:17:39 +0800

    add adventurer adder

---
+++ LinkedListTest/Party.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListTest
+{
+    public class Party
+    {
+        public Adventurer Leader;
+
+        public Party()
+        {
+            Leader = null;
+        }
+
+        public void AddAdventurer(string name, int level)
+        {
+            Adventurer newAdventurer = new Adventurer(name, level);
+        }
+    }
+}
