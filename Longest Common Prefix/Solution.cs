05189a32a75ccac6c64fa26b7c2f69e5392a065d
David Barredo <datdat@rdmsinc.com>
2023-11-21 20:12:40 +0800

    add LongestCommonPrefix solution

--- Longest Common Prefix/Solution.cs
+++ Longest Common Prefix/Solution.cs
@@ -10,9 +10,27 @@ namespace Longest_Common_Prefix
     {
         public string LongestCommonPrefix(string[] strs)
         {
-            
+            if (strs == null || strs.Length == 0)
+            {
+                return string.Empty;
+            }
 
-            return null;
+            int shortestLength = strs.Min(s => s.Length);
+
+            for (int i = 0; i < shortestLength; i++)
+            {
+                char currentChar = strs[0][i];
+
+                if (strs.Any(s => s[i] != currentChar))
+                {
+                    // Stop when a non-matching character is found
+                    return strs[0].Substring(0, i);
+                }
+            }
+
+            // All characters matched, return the entire string
+            return strs[0].Substring(0, shortestLength);
         }
+
     }
 }
