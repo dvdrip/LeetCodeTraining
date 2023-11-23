8286642c8e4eb2756a3507ad39911316da1bd43d
David Barredo <datdat@rdmsinc.com>
2023-11-23 20:08:24 +0800

    add Valid Parenthesis problem

---
+++ Valid Parentheses/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Valid_Parentheses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+
+            Solution solution = new Solution();
+            
+            bool validity = solution.IsValid(s);
+
+            Console.WriteLine(validity);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Valid Parentheses/Solution.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Valid_Parentheses
+{
+    internal class Solution
+    {
+        public bool IsValid(string s)
+        {
+            if (s == null) return false;
+
+            char startChar = s[0];
+            char endChar = s[s.Length - 1];
+
+            if (startChar == '(' && endChar == ')')
+            {
+                return true;
+            }
+            else if (startChar == '{' && endChar == '}')
+            {
+                return true;
+            }
+            else if (startChar == '[' && endChar == ']')
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        static char GetOppositeChar(char inputChar)
+        {
+            switch (inputChar)
+            {
+                case '(':
+                    return ')';
+                case ')':
+                    return '(';
+                case '[':
+                    return ']';
+                case ']':
+                    return '[';
+                case '{':
+                    return '}';
+                case '}':
+                    return '{';
+                // Add more cases as needed
+                default:
+                    return inputChar; // If the input is not a recognized character, return the same character
+            }
+        }
+    }
+}
