f2daa38599452a753c886fe88c2b77c8e6a61441
David Barredo <datdat@rdmsinc.com>
2023-11-24 21:23:44 +0800

    add remove element project

---
+++ Remove Element/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Remove_Element
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 3, 2, 2, 3 };
+            int val = 3;
+
+            Solution solution = new Solution();
+            var x = solution.RemoveElement(nums, val);
+            Console.WriteLine("My out " + x);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Remove Element/Solution.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Remove_Element
+{
+    public class Solution
+    {
+        public int RemoveElement(int[] nums, int val)
+        {
+            List<int> list = new List<int>();
+
+
+
+            foreach (int i in nums)
+            {
+                if (i != val)
+                {
+                    list.Add(i);
+                }
+            }
+
+            int[] myIntArray = list.ToArray();
+
+            for (int i = 0; i < myIntArray.Length; int++)
+            {
+                Console.WriteLine(myIntArray[i]);
+            }
+
+            return myIntArray.Length;
+            
+        }
+    }
+}
