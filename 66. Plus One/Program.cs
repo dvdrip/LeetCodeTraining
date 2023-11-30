f700381d44199fe6f86e80985d933fe8ba3709db
David Barredo <datdat@rdmsinc.com>
2023-12-01 01:43:43 +0800

    add problem 66 Plus One temporary solution error cannot use int32

---
+++ 66. Plus One/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _66.Plus_One
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+
+            int[] myInt = { 9, 8, 7, 6, 5 };
+
+            foreach (var item in solution.PlusOne(myInt)) {
+                Console.Write(item);
+            }
+
+            Console.WriteLine();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 66. Plus One/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _66.Plus_One
+{
+    internal class Solution
+    {
+        public int[] PlusOne(int[] digits)
+        {
+            int fullInt = Convert.ToInt32(string.Join("", digits));
+
+            fullInt++;
+
+            char[] resultChar = fullInt.ToString().ToArray();
+
+            int[] resultArray = resultChar.Select(x => int.Parse(x.ToString())).ToArray();
+
+            return resultArray;
+        }
+    }
+}
