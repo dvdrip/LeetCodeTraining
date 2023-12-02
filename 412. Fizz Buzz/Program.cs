aa106b8adeaf33d52204e739ef55203769fb880e
David Barredo <dvdbarredo@gmail.com>
2023-12-03 01:07:13 +0800

    add 412.Fizz_Buzz problem

---
+++ 412. Fizz Buzz/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _412.Fizz_Buzz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 6;
+
+            Solution solution = new Solution();
+
+            foreach (var item in solution.FizzBuzz(n))
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 412. Fizz Buzz/Solution.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _412.Fizz_Buzz
+{
+    internal class Solution
+    {
+        public IList<string> FizzBuzz(int n)
+        {
+            List<string> stringList = new List<string>();
+
+            for (int i = 1; i < n; i++)
+            {
+                switch (i)
+                {
+                    case int num when num % 3 == 0:
+                        stringList.Add("Fizz");
+                        break;
+                    case int num when num % 5 == 0:
+                        stringList.Add("Buzz");
+                        break;
+                    case int num when num < n:
+                        stringList.Add(i.ToString());
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return stringList;
+        }
+    }
+}
