019c2647967f8333ba9239200490f804fd0ea6e1
David Barredo <datdat@rdmsinc.com>
2023-12-02 19:53:15 +0800

    add Find_Words_That_Can_Be_Formed_by_Characters

---
+++ 1160. Find Words That Can Be Formed by Characters/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1160.Find_Words_That_Can_Be_Formed_by_Characters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = { "cat", "bt", "hat", "tree" };
+            string target = "atach";
+
+            Solution solution = new Solution();
+            var x = solution.CountCharacters(words, target);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 1160. Find Words That Can Be Formed by Characters/Solution.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1160.Find_Words_That_Can_Be_Formed_by_Characters
+{
+    internal class Solution
+    {
+        public int CountCharacters(string[] words, string chars)
+        {
+            int count = 0;
+
+            string uniqueLetters = new string(chars.Distinct().ToArray());
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                for (int j = 0; j < uniqueLetters.Length; j++)
+                {
+                    if (words[i].Contains(chars[j]))
+                    {
+                        count++;
+                    }
+                    else
+                    {
+                        count--;
+                    }
+                }
+            }
+
+            Console.WriteLine(count);
+
+            return count;
+        }
+    }
+}
