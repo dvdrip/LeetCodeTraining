3878e99eb28f939e301bdad6992ad530635e19f3
David Barredo <datdat@rdmsinc.com>
2023-12-02 20:15:31 +0800

    add problem 7 reverse integer

---
+++ 7. Reverse Integer/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7.Reverse_Integer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 123;
+
+            Solution solution = new Solution();
+
+            Console.WriteLine(solution.Reverse(x));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 7. Reverse Integer/Solution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7.Reverse_Integer
+{
+    internal class Solution
+    {
+        public int Reverse(int x)
+        {
+			try
+			{
+                string xString = x.ToString();
+
+                string xStringReversed = new string(xString.Reverse().ToArray());
+
+                int xIntReversed = Convert.ToInt32(xStringReversed);
+
+                return xIntReversed;
+            }
+			catch (Exception ex)
+			{
+                Console.WriteLine(ex.Message);
+                return 0;
+			}
+        }
+    }
+}
