d882c29da0b3ccfb66f0884e9eefc3284f44d4b0
David Barredo <dvdbarredo@gmail.com>
2023-12-06 03:09:19 +0800

    add 67. Add Binary project

---
+++ 67. Add Binary/Solution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _67.Add_Binary
+{
+    public class Solution
+    {
+        public string AddBinary(string a, string b)
+        {
+
+        }
+    }
+}
