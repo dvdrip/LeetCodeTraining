7247b9c4ff40f9301600ac4c01c278e6202b24aa
David Barredo <dvdbarredo@gmail.com>
2023-12-06 02:57:44 +0800

    add 1688.Count_of_Matches_in_Tournament project

---
+++ 1688. Count of Matches in Tournament/Solution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1688.Count_of_Matches_in_Tournament
+{
+    public class Solution
+    {
+        public int NumberOfMatches(int n)
+        {
+            int answer = 0;
+
+            if (n % 2 == 0)
+            {
+                //if even
+
+            }
+            else
+            {
+                //if odd
+
+            }
+
+            return answer;
+        }
+    }
+}
+}
