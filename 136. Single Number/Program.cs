4539e53502b1c4b132e1eebd73a4fa213483f199
David Barredo <dvdbarredo@gmail.com>
2023-12-19 15:16:38 +0800

    add 136._Single_Number solution

---
+++ 136. Single Number/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using _136._Single_Number;
+
+Console.WriteLine("Hello, World!");
+
+Solution solution = new Solution();
+
+solution.SingleNumber(new int[]{ 2, 2, 1 });
+
+Console.ReadLine();
---
+++ 136. Single Number/Solution.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _136._Single_Number
+{
+    public class Solution
+    {
+        public int SingleNumber(int[] nums)
+        {
+            int distinctNum = nums.GroupBy(x => x).Where(y => y.Count() == 1).Select(g => g.Key).FirstOrDefault();
+            return distinctNum;
+        }
+    }
+}
